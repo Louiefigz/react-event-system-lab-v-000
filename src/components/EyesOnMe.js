12cf791834de0f5dd653017093973e64edbbc16e
Aaron Figueroa <figueroa.aaron@gmail.com>
2019-08-27 04:12:23 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component  {
+ 
+    constructor(){
+        super();
+
+        this.good = this.good.bind(this),
+        this.hey = this.hey.bind(this)
+    
+    }
+    
+  good() {
+    console.log('Good!');
+  }  
+    
+  hey() {
+    console.log('Hey! Eyes on me!');
+  }
+ 
+  render() {
+    
+    return (
+    <button onFocus={this.good} onBlur={this.hey}></button>
+    )
+    
+  }
+
+}
+
+module.exports = EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,26 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component  {
+ 
+    constructor(){
+        super();
+
+        this.handleEvent = this.handleEvent.bind(this)
+    
+    }
+  handleEvent() {
+    console.log('Entering password...');
+  }
+ 
+  render() {
+    
+    return (
+    <input type="password" onKeyUp={this.handleEvent}></input>
+    )
+    
+  }
+
+}
+
+module.exports = Keypad;
